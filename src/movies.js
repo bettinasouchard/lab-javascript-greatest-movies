d0a46797946ca24b736e265f4b3476ebfb5225ee
jturqueti <julia.turqueti@gmail.com>
2021-04-18 18:26:07 +0200

    test

--- src/movies.js
+++ src/movies.js
@@ -1,73 +1,119 @@
 // Iteration 1: All directors? - Get the array of all directors.
 
-const getAllDirectors = function(movies) {
-  return movies.map((movie) => { return movie.director; });
-      };
+/**
+ * returns an array containing an array with all the directors
+ * @param {Array} list
+ * @returns {Array} with directors
+ */
 
+ const getAllDirectors = function (array) {
+  return array.map((arr) => {
+    return arr.director;
+  });
+};
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
-const howManyMovies = function(movies) {
-   const stevenSpielMovies = movies.filter((movie) => {return movie.director === "Steven Spielberg" && movie.genre.includes('Drama');});
-   return stevenSpielMovies.length;
-}
+/**
+ * returns an array with all spielberg's drama movies 
+ * @param {Array} list
+ * @returns {Array} with Spielberg Drama Movies
+ */
 
+
+const howManyMovies = function (array) {
+  const stevenSpielMovies = array.filter((movie) => {
+    return (
+      movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+    );
+  });
+  return stevenSpielMovies.length;
+};
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
-function ratesAverage(moviesss) {
-    if (moviesss.length === 0) return 0;
-    const sumRate  = moviesss.reduce((a,movie) => {return (movie.rate === undefined ? a : a+=movie.rate );
-},0);
-return (Number((sumRate/moviesss.length).toFixed(2)));
-}
+/**
+ * returns an array with all spielberg's drama movies 
+ * @param {Array} list
+ * @returns {Number} global rate of the movies list with 2 decimals
+ */
 
+
+function ratesAverage(array) {
+  if (array.length === 0) return 0;
+  const sumRate = array.reduce((a, movie) => {
+    return movie.rate === undefined ? a : (a += movie.rate);
+  }, 0);
+  return Number((sumRate / array.length).toFixed(2));
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
-    function dramaMoviesRate(movielist) {
-        
-        const drama = movielist.filter(function (movie) {
-            return ( movie.genre.includes('Drama'));
-        });
-            
-        let total = 0
-        let result = drama.map((x) => total += x.rate );
-        if (drama.length === 0) return total;
-        return (Number((total/drama.length).toFixed(2)))
-      }
+/**
+ * returns an array with all spielberg's drama movies 
+ * @param {Array} list
+ * @returns {Number} global rate of the drama movies list with 2 decimals
+ */
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
-function orderByYear (movies) {
-     
-	let a = (movies.slice().sort((function(a,b) { 
-        let value =	(a.year == b.year) ? (a.title > b.title ? 1 : -1) : (a.year> b.year? 1 : -1);      
-        return value;
-      }))); 
-    
-    
-    return a;
+function dramaMoviesRate(movieList) {
+  const drama = movieList.filter(function (movie) {
+    return movie.genre.includes("Drama");
+  });
+  let total = 0;
+  let result = drama.map((x) => (total += x.rate));
+  if (drama.length === 0) return total;
+  return Number((total / drama.length).toFixed(2));
 }
 
+// Create a dramaMoviesRate() function that receives an array
+// as a parameter to get the average rate of all drama movies!
+// Let's see if it is better than the general average.
+//
+// Again, rounded to 2 decimals!
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
 
+/**
+ * returns an array with all spielberg's drama movies 
+ * @param {Array} list
+ * @returns {Array} movies list in ascending order by their release year
+ */
+
+function orderByYear(array) {
+  let a = array.slice().sort(function (a, b) {
+    let value =
+      a.year == b.year
+        ? a.title > b.title
+          ? 1
+          : -1
+        : a.year > b.year
+        ? 1
+        : -1;
+    return value;
+  });
+  return a;
+}
+
+orderByYear(movies)
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
-function orderAlphabetically (movies) { 
-    let film = movies.map((movie) => { return movie.title; });
-    let twenty = film.sort();   
-    return twenty.slice(0,20)   
+/**
+ * returns an array with all spielberg's drama movies 
+ * @param {Array} list
+ * @returns {Array} top 20 movies list in alphabetic order
+ */
+
+function orderAlphabetically(array) {
+  let film = array.map((movie) => {
+    return movie.title;
+  });
+  let sorting = film.sort();
+  return sorting.slice(0, 20);
 }
-
-
-//function orderAlphabetically (movies){
- //   return movies.sort((function(x, y){
- //       return x.toString().localeCompare(y.toString());
- //   }));
-// }
+orderAlphabetically(movies);
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
